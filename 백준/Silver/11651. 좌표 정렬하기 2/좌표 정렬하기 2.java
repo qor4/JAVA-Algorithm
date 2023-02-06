1620cc79c0e4628f72a1ba0ffa28b429fbc22529
daeun <58421346+qor4@users.noreply.github.com>
2023-02-06 17:39:20 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 824 ms, Memory: 51908 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+class Coor {
+	int x;
+	int y;
+}
+
+class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+		List<Coor> coors = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			Coor c = new Coor();
+			c.x = Integer.parseInt(st.nextToken());
+			c.y = Integer.parseInt(st.nextToken());
+			coors.add(c);
+		}
+		Collections.sort(coors, new Comparator<Coor>() {
+			@Override
+			public int compare(Coor o1, Coor o2) {
+				if (o1.y == o2.y) {
+					return o1.x - o2.x;
+				}
+				return o1.y - o2.y;
+			}
+		});
+		for (int i = 0; i < coors.size(); i++) {
+			bw.write(coors.get(i).x + " " + coors.get(i).y + "\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+
+}
