9456e637642b73a720a5deab3e69219038a71ad6
daeun <58421346+qor4@users.noreply.github.com>
2023-02-08 16:17:35 +0900

    [Bronze II] Title: 블랙잭, Time: 132 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,42 +1,37 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		String[] str = br.readLine().split(" ");
-		int N = Integer.parseInt(str[0]);
-		int M = Integer.parseInt(str[1]);
-
-		List<String> nums = Arrays.asList(br.readLine().split(" "));
-		List<Integer> newNums = new ArrayList<>();
-		List<Integer> sumNums = new ArrayList<>();
-
-		for (String num : nums) {
-			newNums.add(Integer.parseInt(num));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] nums = new int[N];
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
 		}
-
+		int max = -1;
 		for (int i = 0; i < N - 2; i++) {
 			for (int j = i + 1; j < N - 1; j++) {
 				for (int k = j + 1; k < N; k++) {
-					int sum = newNums.get(i) + newNums.get(j) + newNums.get(k);
-					sumNums.add(sum);
+					if (nums[i] + nums[j] + nums[k] <= M && nums[i] + nums[j] + nums[k] > max) {
+						max = nums[i] + nums[j] + nums[k];
+					}
 				}
 			}
 		}
-
-		Collections.sort(sumNums, Collections.reverseOrder());
-		for (Integer num : sumNums) {
-			if (num <= M) {
-				System.out.println(num);
-				break;
-			}
-		}
+		bw.write(max + "");
+		bw.flush();
+		bw.close();
 	}
 }
