850273193ae32cfbf5026a89c24028eeab21fedb
daeun <58421346+qor4@users.noreply.github.com>
2023-02-08 14:26:20 +0900

    [Silver III] Title: N과 M (8), Time: 220 ms, Memory: 18556 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static int[] numbers, inputs;
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		numbers = new int[M];
+		inputs = new int[N];
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			inputs[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(inputs);
+		perm(0, 0);
+		bw.flush();
+		bw.close();
+	}
+
+	private static void perm(int cnt, int start) throws IOException {
+		if (cnt == M) {
+			for (int i = 0; i < M; i++) {
+				bw.write(numbers[i] + " ");
+			}
+			bw.write("\n");
+			return;
+		}
+		for (int i = start; i < N; i++) {
+			numbers[cnt] = inputs[i];
+			perm(cnt + 1, i);
+		}
+
+	}
+}
