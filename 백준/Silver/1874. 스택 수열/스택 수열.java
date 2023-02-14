a03d04449ce528dfcfbd85d99678a56667f04e60
daeun <58421346+qor4@users.noreply.github.com>
2023-02-14 12:01:29 +0900

    [Silver II] Title: 스택 수열, Time: 364 ms, Memory: 27028 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+		int[] numbers = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			numbers[i] = Integer.parseInt(br.readLine());
+		}
+
+		Stack<Integer> stack = new Stack<>();
+		StringBuffer sb = new StringBuffer();
+		int idx = 0;
+		int num = 1;
+		while (idx < N) {
+			if (stack.isEmpty()) {
+				stack.add(num);
+				sb.append("+\n");
+				num++;
+			}
+			if (stack.peek() == numbers[idx]) {
+				sb.append("-\n");
+				stack.pop();
+				idx++;
+			} else {
+				stack.add(num);
+				sb.append("+\n");
+				num++;
+			}
+			if (num > N + 1) {
+				bw.write("NO");
+				break;
+			}
+		}
+		if (num <= N + 1) {
+			bw.write(sb.toString());
+		}
+		bw.flush();
+		bw.close();
+
+	}
+}
